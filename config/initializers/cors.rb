ea90743df3b02e7a2e739ec873b525408c99bb50
potatoman-dev <enakatani122@gmail.com>
2024-08-25 11:02:46 +0900

    corsのポート番号修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'https://tamagokakegohan.com/'
+    origins 'localhost:3030', 'https://tamagokakegohan.com/'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
