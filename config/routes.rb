845e3fa2a6e01fdbaa961a5a36cbe1753c8c8a99
potatoman-dev <enakatani122@gmail.com>
2024-08-28 00:27:09 +0900

    deviseの導入とログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  # mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v1 do
       get "status", to: "status#index"
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
     end
   end
 end
