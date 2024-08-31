510ccf25d3f2dfb9bfe30342386738b7e60df06b
potatoman-dev <enakatani122@gmail.com>
2024-09-01 01:10:45 +0900

    recipe image用uploaderの追加 #8

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,6 @@ class Recipe < ApplicationRecord
   validates :cooking_time, numericality: { only_integer: true }
   validates :status, presence: true
   enum status: { draft: 0, published: 1 }
+
+  mount_uploader :image, RecipeImageUploader
 end
