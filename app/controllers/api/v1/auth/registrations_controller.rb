1c34e1d60da242e60be6af6b380ceffd217f5ad3
potatoman-dev <enakatani122@gmail.com>
2024-08-31 16:34:35 +0900

    registrationsの登録更新メソッドをprivateに設定

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  private
   def sign_up_params
     params.permit(:email, :password, :password_confirmation, :name, :avatar)
   end
