1a6a6a781de93c57fa990b498dfff395cc8298f5
potatoman-dev <enakatani122@gmail.com>
2024-09-04 21:58:16 +0900

    recipe一覧取得にserializerを追加 #17

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::RecipesController < ApplicationController
 
   def index
     recipes = Recipe.all
-    render json: recipes, status: :ok
+    render json: recipes, each_serializer: RecipeSerializer, status: :ok
   end
 
   def show
