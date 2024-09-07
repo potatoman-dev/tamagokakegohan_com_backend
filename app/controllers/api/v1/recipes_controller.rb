54fea898689b5a530cd69cd65721a124ee201864
potatoman-dev <enakatani122@gmail.com>
2024-09-07 17:29:22 +0900

    update時、publishedの場合にstepsまたはingredientsが空なら例外を発生 #24

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -40,7 +40,13 @@ class Api::V1::RecipesController < ApplicationController
 
   def create
     Recipe.transaction do
-      @recipe = @user.recipes.build(basic_recipe_params)
+      @recipe = @user.recipes.build(
+        title: params.require(:recipe)[:title],
+        body: params.require(:recipe)[:body],
+        cooking_time: params.require(:recipe)[:cooking_time].to_i,
+        image: params.require(:recipe)[:image],
+        status: params.require(:recipe)[:status]
+      )
 
       if @recipe.save
         # steps
@@ -83,87 +89,98 @@ class Api::V1::RecipesController < ApplicationController
     @recipe = @user.recipes.find(params[:id])
 
     ActiveRecord::Base.transaction do
-      if @recipe.update(basic_recipe_params)
+      # publishedの場合はstepsとingredientsがないと例外を発生させる
+      if recipe_params[:status] == 'published' && (recipe_params[:steps].blank? || recipe_params[:ingredients].blank?)
+        render json: { errors: 'must have at least one step and one ingredient.' }, status: :unprocessable_entity
+        raise ActiveRecord::Rollback
+      end
+
+      if @recipe.update(
+        title: params.require(:recipe)[:title],
+        body: params.require(:recipe)[:body],
+        cooking_time: params.require(:recipe)[:cooking_time].to_i,
+        image: params.require(:recipe)[:image],
+        status: params.require(:recipe)[:status]
+      )
         # steps
         steps_attributes = recipe_params[:steps].to_h.values.map.with_index { |step, index|
-            {
-              recipe_id: @recipe.id,
+          {
+            recipe_id: @recipe.id,
             step_number: index + 1,
-              instruction: step[:instruction],
-              image: step[:image]
-            }
+            instruction: step[:instruction],
+            image: step[:image]
           }
+        }
 
-          # 存在しているかどうか
+        # 存在しているかどうか
         new_step_numbers = steps_attributes.map.with_index { |attr, index|
           index + 1
-          }
-          # 不要なステップを削除
-          @recipe.steps.where.not(step_number: new_step_numbers).destroy_all
+        }
+        # 不要なステップを削除
+        @recipe.steps.where.not(step_number: new_step_numbers).destroy_all
 
         steps_attributes.each_with_index do |attr, index|
-            # 存在している場合
+          # 存在している場合
           existing_step = @recipe.steps.find_by(step_number: index + 1)
 
-            if existing_step
-              # 更新
-              existing_step.update!(
-                instruction: attr[:instruction],
-                image: attr[:image]
-                )
-            else
-              # 追加
-              @recipe.steps.create!(
+          if existing_step
+            # 更新
+            existing_step.update!(
+              instruction: attr[:instruction],
+              image: attr[:image]
+              )
+          else
+            # 追加
+            @recipe.steps.create!(
               step_number: index + 1,
-                instruction: attr[:instruction],
-                image: attr[:image]
-              )
-            end
+              instruction: attr[:instruction],
+              image: attr[:image]
+            )
           end
+        end
 
-        # ingredients
-          ingredients_attributes = recipe_params[:ingredients].to_h.values.map.with_index { |recipe_ingredient, index|
-          ingredient = Ingredient.find_or_create_by(name: recipe_ingredient[:name])
+      # ingredients
+        ingredients_attributes = recipe_params[:ingredients].to_h.values.map.with_index { |recipe_ingredient, index|
+        ingredient = Ingredient.find_or_create_by(name: recipe_ingredient[:name])
 
-            {
-              recipe_id: @recipe.id,
-              ingredient_id: ingredient.id,
+          {
+            recipe_id: @recipe.id,
+            ingredient_id: ingredient.id,
+            ingredient_number: index + 1,
+            amount: recipe_ingredient[:amount]
+          }
+        }
+
+        # 存在しているかどうか
+        new_ingredient_numbers = ingredients_attributes.map.with_index { |attr, index|
+          index + 1
+        }
+        # 不要なrecipe_ingredientを削除
+        @recipe.recipe_ingredients.where.not(ingredient_number: new_ingredient_numbers).destroy_all
+
+        ingredients_attributes.each_with_index do |attr, index|
+          # 存在している場合
+          existing_ingredient = @recipe.recipe_ingredients.find_by(ingredient_number: index + 1)
+
+          if existing_ingredient
+            # 更新
+            existing_ingredient.update!(
+              ingredient_id: attr[:ingredient_id],
+              amount: attr[:amount]
+            )
+          else
+            # 追加
+            @recipe.recipe_ingredients.create!(
+              ingredient_id: attr[:ingredient_id],
               ingredient_number: index + 1,
-              amount: recipe_ingredient[:amount]
-            }
-          }
-
-          # 存在しているかどうか
-          new_ingredient_numbers = ingredients_attributes.map.with_index { |attr, index|
-            index + 1
-          }
-          # 不要なrecipe_ingredientを削除
-          @recipe.recipe_ingredients.where.not(ingredient_number: new_ingredient_numbers).destroy_all
-
-          ingredients_attributes.each_with_index do |attr, index|
-            # 存在している場合
-            existing_ingredient = @recipe.recipe_ingredients.find_by(ingredient_number: index + 1)
-
-
-            if existing_ingredient
-              # 更新
-              existing_ingredient.update!(
-                ingredient_id: attr[:ingredient_id],
-                amount: attr[:amount]
-              )
-            else
-              # 追加
-              @recipe.recipe_ingredients.create!(
-                ingredient_id: attr[:ingredient_id],
-                ingredient_number: index + 1,
-                amount: attr[:amount]
-              )
-            end
+              amount: attr[:amount]
+            )
           end
+        end
 
         render json: @recipe, serializer: RecipeSerializer,  status: :ok
       else
-        render json: @recipe.errors, status: :unprocessable_entity
+        render json: { errors: @recipe.errors.full_messages }, status: :unprocessable_entity
       end
     rescue ActiveRecord::RecordInvalid => e
       render json: { errors: e.record.errors }, status: :unprocessable_entity
@@ -184,16 +201,6 @@ class Api::V1::RecipesController < ApplicationController
     @user = current_api_v1_user
   end
 
-  def basic_recipe_params
-    params.require(:recipe).permit(
-        :title,
-        :body,
-        :cooking_time,
-        :image,
-        :status
-      )
-  end
-
   def recipe_params
     params.require(:recipe).permit(
         :title,
@@ -201,7 +208,7 @@ class Api::V1::RecipesController < ApplicationController
         :cooking_time,
         :image,
         :status,
-        steps: [:step_number, :instruction, :image],
+        steps: [:instruction, :image],
         ingredients: [:name, :amount]
       )
   end
