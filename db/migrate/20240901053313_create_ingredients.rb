faf2644b562a0f92d09d6e1025efc907cbd9c62a
potatoman-dev <enakatani122@gmail.com>
2024-09-08 23:42:19 +0900

    update migration file

--- db/migrate/20240901053313_create_ingredients.rb
+++ db/migrate/20240901053313_create_ingredients.rb
@@ -1,7 +1,6 @@
 class CreateIngredients < ActiveRecord::Migration[7.0]
   def change
     create_table :ingredients do |t|
-      t.references :category, null: false, foreign_key: true
       t.string :name, null: false
 
       t.timestamps
