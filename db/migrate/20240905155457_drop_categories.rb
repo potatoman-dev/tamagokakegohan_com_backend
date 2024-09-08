2a545f66b45f9181f038409185a1b355bf261c78
potatoman-dev <enakatani122@gmail.com>
2024-09-09 00:04:38 +0900

    update migration file

--- db/migrate/20240905155457_drop_categories.rb
+++
@@ -1,9 +0,0 @@
-class DropCategories < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :categories do |t|
-      t.string "name", null: false
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-    end
-  end
-end
