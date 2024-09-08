83b147a314e383c2f48f7c863e33cab8b96b4d9a
potatoman-dev <enakatani122@gmail.com>
2024-09-08 16:25:28 +0900

    recipeのシリアライザーにbookmark_countとcreated_atを追加 #30

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,6 +1,10 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :cooking_time, :image, :status
+  attributes :id, :title, :body, :cooking_time, :image, :status, :bookmark_count, :created_at
   has_many :steps, serializer: StepSerializer
   has_many :recipe_ingredients, serializer: RecipeIngredientSerializer
   belongs_to :user, serializer: UserSummarySerializer
+
+  def bookmark_count
+    object.bookmarks.count
+  end
 end
