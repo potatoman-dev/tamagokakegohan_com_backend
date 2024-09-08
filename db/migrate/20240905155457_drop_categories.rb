1c218f924056b836f55c05d1866e5e6d904f0af2
potatoman-dev <enakatani122@gmail.com>
2024-09-08 23:48:42 +0900

    update migration file

--- db/migrate/20240905155457_drop_categories.rb
+++ db/migrate/20240905155457_drop_categories.rb
@@ -1,8 +1,5 @@
 class DropCategories < ActiveRecord::Migration[7.0]
   def change
-    remove_foreign_key :ingredients, :categories
-    remove_reference :ingredients, :category, index: true
-
     drop_table :categories do |t|
       t.string "name", null: false
       t.datetime "created_at", null: false
