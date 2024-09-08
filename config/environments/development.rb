fc3c3390bf91fb4830fe64ac1d7676051f259f72
potatoman-dev <enakatani122@gmail.com>
2024-09-08 19:23:51 +0900

    host情報の追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << "htamagokakegohan-com-backend.onrender.com"
+  config.hosts << "tamagokakegohan-com-backend.onrender.com"
+  config.hosts << "'tamagokakegohan.com"
+  config.hosts << "xn--u8jmhc5czfqe0h.com"
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
